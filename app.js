2260e6ce3ebe84a94740ade58c73910300982060
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-09-01 19:13:52 +0100

    cleaning up for reinstall

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors({
-  origin: 'https://www.emileholba.co.uk'
+  origin: 'https://gawainhewitt.co.uk'
 }));
 
 app.get('/', (req, res) => {
